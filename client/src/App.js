1c423b9b5fbcb561794042d6921cd391060ad7cf
alisbah6 <alisbahhina@gmail.com>
2025-02-12 15:38:37 +0530

    Updated_App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import AllRoutes from './AllRoutes';
+// import AllRoutes from './AllRoutes';
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Auth from './pages/Auth/Auth'
+import Questions from './pages/Questions/Questions'
+import AskQuestion from './pages/AskQuestion/AskQuestion'
+import { DisplayQues } from './pages/Questions/DisplayQues'
+import Tags from './pages/Tags/Tags'
+import Users from './pages/Users/Users'
+import UserProfile from './pages/UserProfile/UserProfile'
 import {BrowserRouter as Router} from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -16,7 +25,16 @@ function App() {
     <div className="App">  
     <Router>
     <Navbar/>
-    <AllRoutes/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/Auth' element={<Auth/>}></Route>
+      <Route path='/Questions' element={<Questions/>}/>
+      <Route path='/AskQuestion' element={<AskQuestion/>}/>
+      <Route path='/Questions/:id' element={<DisplayQues/>}></Route>
+      <Route path='/Tags' element={<Tags/>}/>
+      <Route path='/Users' element={<Users/>}/>
+      <Route path='/Users/:id' element={<UserProfile/>}/>
+    </Routes>
     </Router> 
     </div>
   );
